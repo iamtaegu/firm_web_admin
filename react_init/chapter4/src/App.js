80dfbfb31ca29fd2b9c0c9f669ff7b0d79112aba
Taegu <gmdals45@gmail.com>
2023-07-09 18:28:38 +0900

    [2023/07/09][ch5] 컴포넌트 분리

--- react_init/chapter4/src/App.js
+++ react_init/chapter4/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header"
+import Body from "./component/Body"
+import Footer from "./component/Footer"
 
 /**
  * 리액트이 컴포넌트는
  * HTML을 반환하는 메소드를 칭함
+ *
+ * <App> 루트 컴포넌트
+ * <Header> 자식 컴포넌트를 배치
  */
 function App() {
   return (
     <div className="App">
-      <h2>Hi</h2>
+        <Header />
+        <Body />
+        <Footer />
     </div>
   );
 }
---
+++ react_init/chapter4/src/component/Header.js
@@ -0,0 +1,18 @@
+/*function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+}*/
+
+// 리액트 컴포넌트와 HTML 태그를 구분하기 위해 항상 대문자로 시작
+const Header = () => {
+    return (
+        <header>
+            <h1>header</h1>
+        </header>
+    );
+};
+
+export default Header;
