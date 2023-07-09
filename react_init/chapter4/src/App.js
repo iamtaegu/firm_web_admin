0152e37b28045bb50a0dc405410a4e4575ec15c2
Taegu <gmdals45@gmail.com>
2023-07-09 16:08:05 +0900

    [2023/07/08][ch4] add 리액트 앱:

---
+++ react_init/chapter4/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+/**
+ * 리액트이 컴포넌트는
+ * HTML을 반환하는 메소드를 칭함
+ */
+function App() {
+  return (
+    <div className="App">
+      <h2>Hi</h2>
+    </div>
+  );
+}
+
+export default App;
