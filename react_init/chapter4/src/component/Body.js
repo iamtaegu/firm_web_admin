de095e8bf5f09e8f17abbb1217fb4b7d7a0a7f0a
iamtaegu <gmdals45@gmail.com>
2023-07-12 08:25:17 +0900

    [2023/07/12][react] 이벤트 처리

--- react_init/chapter4/src/component/Body.js
+++ react_init/chapter4/src/component/Body.js
@@ -1,6 +1,7 @@
 import "./Body.css"
+import { useState } from "react";
 
-const Body = ({name, location, favorities}) => {
+/*const Body = ({name, location, favorities}) => {
     //const {name, location} = props;
     return (
         <div className="body">
@@ -11,6 +12,44 @@ const Body = ({name, location, favorities}) => {
 
 Body.defaultProps = {
     favorities: [],
+}
+*/
+
+// 이벤트 처리하기
+/*function Body() {
+    function handleOnClick(e) {
+        console.log(e);
+        console.log(e.target.name);
+    }
+
+    return (
+        <div className="body">
+            <button name="A버튼" onClick={handleOnClick} >
+                A버튼
+            </button>
+            <button name="B버튼" onClick={handleOnClick} >
+                B버튼
+            </button>
+        </div>
+    );
+}*/
+
+function Body() {
+
+    console.log("컴포넌트 업데이트 ! ");
+
+    const [count, setCount] = useState(0);
+
+    const onIncerease = () => {
+        setCount(count+1);
+    }
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncerease}> + </button>
+        </div>
+    )
 }
 
 export default Body;
